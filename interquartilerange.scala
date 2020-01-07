b3a5eae5384b29ca47d82054e2f9ddbdb2f16ab8
Zeeshan Haider <zeeshanalisyed@users.noreply.github.com>
2020-01-07 16:46:53 +0400

    Update interquartilerange.scala

--- interquartilerange.scala
+++ interquartilerange.scala
@@ -2,32 +2,19 @@ import java.io.InputStream
 import scala.io.Source
 
 
-object Test {
+object Solution {
 
   def median(elements: List[Float],n: Int): Float = {
-    if (n % 2 == 1) elements(n / 2)
+    if (n % 2 >= 1) elements(n / 2)
     else {
       val (up, down) = elements.splitAt(n / 2)
       (up.last + down.head) / 2
     }
   }
-  def getMiddle(numbers: List[Float], n: Int): Float = median(numbers, numbers.size)
-
-  def getRanges(numbers: List[Float], _median: Float, n: Int): (List[Float], List[Float]) = {
-    var (lower, upper) = numbers.partition(_ < _median)
+  def generateQuartileLists(n: List[Float]): (List[Float], List[Float]) = {
+    var (lower, upper): (List[Float], List[Float]) = n.splitAt(n.size/2)
     upper = upper.drop(1)
     (lower, upper)
-  }
-
-  def quartile(numbers: List[Float], n: Int): (Float, Float, Float) = {
-    val _median: Float = getMiddle(numbers, n)
-    val (lower, upper) = getRanges(numbers, _median, n)
-    val _lower: Float = median(lower, lower.size)
-    var _upper: Float = median(upper, upper.size)
-    if (n % 2 == 0) {
-      _upper = _upper - 1
-    }
-    return (_lower, _median, _upper);
   }
   def generateFrequencyData(n: List[Float], f: List[Int]): List[Float] = {
     var list: List[Float] = List[Float]()
@@ -44,7 +31,7 @@ object Test {
     var numbers: List[Float] = lines.next().mkString.split(" ").map(_.toFloat).toList
     var frequecny: List[Int] = lines.next().mkString.split(" ").map(_.toInt).toList
     val data: List[Float] = generateFrequencyData(numbers, frequecny);
-    val (lower, middle, upper) = quartile(data, data.size)
-    println(upper - lower)
+    val (lower, upper) = generateQuartileLists(data)
+    print((median(upper, upper.size) - median(lower, lower.size)).floor)
   }
 }
